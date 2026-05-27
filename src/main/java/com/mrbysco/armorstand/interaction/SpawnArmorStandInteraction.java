b3c37d175be48425dc4d8c108d36eaf34fccdf2c
Mrbysco <Mrbysco@users.noreply.github.com>
2026-05-27 20:03:48 +0200

    Fix imports

--- src/main/java/com/mrbysco/armorstand/interaction/SpawnArmorStandInteraction.java
+++ src/main/java/com/mrbysco/armorstand/interaction/SpawnArmorStandInteraction.java
@@ -7,9 +7,7 @@ import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.math.util.ChunkUtil;
-import com.hypixel.hytale.math.vector.Vector3d;
-import com.hypixel.hytale.math.vector.Vector3f;
-import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.math.vector.Rotation3f;
 import com.hypixel.hytale.protocol.InteractionType;
 import com.hypixel.hytale.server.core.asset.type.blockhitbox.BlockBoundingBoxes;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
@@ -28,6 +26,8 @@ import com.hypixel.hytale.server.core.universe.world.npc.INonPlayerCharacter;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.NPCPlugin;
 import it.unimi.dsi.fastutil.Pair;
+import org.joml.Vector3d;
+import org.joml.Vector3i;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -52,14 +52,14 @@ public class SpawnArmorStandInteraction extends SimpleBlockInteraction {
 		if (itemStack == null) return;
 
 		Ref<EntityStore> ref = context.getEntity();
-		Vector3d vector3d = targetBlock.toVector3d();
+		Vector3d vector3d = new Vector3d(targetBlock);
 		vector3d.add(0.5, 0.5, 0.5);
 
-		Vector3f rotation = new Vector3f();
+		Rotation3f rotation = new Rotation3f();
 		HeadRotation headRotation = commandBuffer.getComponent(ref, HeadRotation.getComponentType());
 		if (headRotation != null) {
 			//Invert the yaw by 180 degrees to make the armor stand face the player
-			rotation.setYaw(headRotation.getRotation().getYaw() + (float) (Math.PI / 180.0) * 180.0F);
+			rotation.setYaw(headRotation.getRotation().yaw() + (float) (Math.PI / 180.0) * 180.0F);
 		}
 
 		WorldChunk worldchunk = world.getChunkIfInMemory(ChunkUtil.indexChunkFromBlock(targetBlock.x, targetBlock.z));
@@ -75,7 +75,7 @@ public class SpawnArmorStandInteraction extends SimpleBlockInteraction {
 		}
 	}
 
-	private void spawnArmorStand(@Nonnull Store<EntityStore> store, @Nonnull Vector3d position, @Nonnull Vector3f rotation) {
+	private void spawnArmorStand(@Nonnull Store<EntityStore> store, @Nonnull Vector3d position, @Nonnull Rotation3f rotation) {
 		String npcType = this.isLight ? "Armor_Stand_Light" : "Armor_Stand";
 		Pair<Ref<EntityStore>, INonPlayerCharacter> pair = NPCPlugin.get().spawnNPC(store, npcType, null, position, rotation);
 		if (pair != null) {
